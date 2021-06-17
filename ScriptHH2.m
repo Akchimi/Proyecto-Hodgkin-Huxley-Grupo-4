355bd13e611f9773fd0f0ca901397ab31687def0
Akchimi <80069734+Akchimi@users.noreply.github.com>
2021-06-16 21:16:34 -0600

    Update ScriptHH2.m

--- ScriptHH2.m
+++ ScriptHH2.m
@@ -1,7 +1,7 @@
-% Proyecto: Simulaci髇 del m骴elo de Hodgkin Huxley 
+% Proyecto: Simulaci贸n del m贸delo de Hodgkin Huxley 
 %
 % Valeria Bonilla Rojas
-% Johnny Borb髇 Valverde
+% Johnny Borb贸n Valverde
 % Angello Crawford Clark
 % Daniel Espinoza Castro
 %
@@ -15,7 +15,7 @@ g(2)=120;
 g(3)=0.3;
 % Area (cm2) usando el radio (a) mencionado en el paper de H-H
 A = (0.0238)^2*pi;
-% Corriente maxima (uA)
+% Corriente maxima (mA)
 Imax = 0.1;
 % Potenciales de equilibrio (mV) E:(1=K, 2=Na, 3=L)
 E(1)=-12; 
@@ -31,9 +31,9 @@ x(1) = 0.318;
 x(2) = 0.053; 
 x(3)=0.6;
 t_i=0;
-% Paso para realizar la integraci髇
+% Paso para realizar la integraci贸n
 dt=0.01;
-% Se establece la duracion total de la simulaci髇 (y por tanto, el eje x del gr醘ico)
+% Se establece la duracion total de la simulaci贸n (y por tanto, el eje x del gr谩fico)
 t_f = 200;
 % Implementacion del metodo de Euler para integrar el sistema
 V = V0;
@@ -53,7 +53,7 @@ alfa(3)=0.07*exp((-V)/20);
 beta(1)=0.125*exp((-V)/80);
 beta(2)=4*exp((-V)/18);
 beta(3)=1/(exp((30-V)/10)+1);
-% Actualizaci髇 del vector de parametros n, m y h
+% Actualizaci贸n del vector de parametros n, m y h
 x = x.*(1-dt.*(alfa+beta)) + alfa.*dt;
 % Calculo de las conductancias actualizadas
 gnmh(1)=g(1)*x(1)^4;
@@ -61,14 +61,14 @@ gnmh(2)=g(2)*x(2)^3*x(3);
 gnmh(3)=g(3);
 % Calculo de la suma de corrientes total
 I=gnmh.*(V-E);
-% Actualizaci髇 del potencial de membrana con el m閠odo de Euler
+% Actualizaci贸n del potencial de membrana con el m茅todo de Euler
 V=V+dt/C*(I_ext-sum(I));
-% Guardado de las variables para la gr醘ica
+% Guardado de las variables para la gr谩fica
 t_i=t_i+1;
 x_plot(t_i)=t;
 y_plot(t_i)=V;
 end
-% Gr醘ica
+% Gr谩fica
 figure(1);hold off;plot(x_plot,y_plot,'red');
 xlabel('Tiempo (ms)'); ylabel('Potencial de membrana (mV)');
 title('Potencial de la membrana vs Tiempo');
